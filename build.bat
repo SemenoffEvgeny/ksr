b65dd37e2cb4f60a7dc8ae03a7b0d7cf009b3efe
Omikronikus <johny9763@gmail.com>
2019-06-06 21:26:34 +0600

    views fix

--- build.bat
+++ build.bat
@@ -1,11 +1,10 @@
-call mvn clean package
+call mvn package
 set CURRENT_DIR_PATH=%~dp0
 set CURRENT_DIR_PATH=%CURRENT_DIR_PATH:~0,-1%
 for %%a in ("%CURRENT_DIR_PATH%") do set PROJECT_NAME=%%~na
-cd target
 echo @echo off > "%PROJECT_NAME%".bat
 echo.>> "%PROJECT_NAME%".bat
-echo java -jar coffeeShop-1.5.3.RELEASE.jar >> "%PROJECT_NAME%".bat
+echo java -jar target/coffeeShop-1.5.3.RELEASE.jar >> "%PROJECT_NAME%".bat
 echo.>> "%PROJECT_NAME%".bat
 
 
